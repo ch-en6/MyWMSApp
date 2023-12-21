5eb38bdc2f82fcedee57aa9e5829d1bdf2252d38
hymmmmm <1874287104@qq.com>
2023-12-21 22:56:37 +0800

    增加个人信息相关功能

---
+++ wmsApp/dialog/DeliverDialog.xaml.cs
@@ -0,0 +1,40 @@
+﻿using ModernWpf.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Web.UI.MobileControls;
+using System.Windows;
+using WindowsFormsApp1.dto;
+using wms;
+using wms.pojo;
+using wmsApp.controls;
+using wmsApp.pojo;
+
+namespace wmsApp.dialog
+{
+    /// <summary>
+    /// DeliverDialog.xaml 的交互逻辑
+    /// </summary>
+    public partial class DeliverDialog : ContentDialog
+    {
+        public DeliverDialog()
+        {
+            InitializeComponent();   
+            List<Deliver> list = new List<Deliver>();
+            datagrid.ItemsSource = list;
+        }
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+
+        }
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+
+        }
+
+        private void datagrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ wmsApp/pojo/Deliver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wmsApp.pojo
+{
+    public class Deliver
+    {
+
+        public long DeliverId { get; set; }
+
+        public long HouseId { get; set; }
+
+        public DateTime DeliverTime { get; set; }
+
+        public long MaterialId { get; set; }
+
+        public int DeliverCount { get; set; }
+
+        public long UserId { get; set; }
+
+        public string Notes { get; set; }
+
+        public int Stock { get; set; }
+
+        public Deliver(long deliverId, long houseId, DateTime deliverTime, long materialId, int deliverCount, long userId, string notes, int stock)
+        {
+            DeliverId = deliverId;
+            HouseId = houseId;
+            DeliverTime = deliverTime;
+            MaterialId = materialId;
+            DeliverCount = deliverCount;
+            UserId = userId;
+            Notes = notes;
+            Stock = stock;
+        }
+
+        public Deliver()
+        {
+        }
+    }
+}
