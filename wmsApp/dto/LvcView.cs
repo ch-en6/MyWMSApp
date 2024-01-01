8cc72570c63a29cdd3ba9b2a1bdc4514a606363c
hymmmmm <1874287104@qq.com>
2024-01-02 02:25:31 +0800

    111

---
+++ wmsApp/dto/LvcView.cs
@@ -0,0 +1,18 @@
+﻿using LiveCharts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wms.dto
+{
+    public class LvcView
+    {
+        public ChartValues<int> ChartDatas { get; set; }
+        public List<string> XLables { get; set; }
+        
+    }
+}
